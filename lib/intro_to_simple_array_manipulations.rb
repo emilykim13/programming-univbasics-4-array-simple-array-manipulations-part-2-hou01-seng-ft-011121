cbdc00e1fe6122d4e2a0ae93fb5eda8a471c38ba
Emily Nguyen <emilyn0614@gmail.com>
2021-01-03 16:37:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,7 @@
 def using_concat(array1, array2)
   @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   more_favs = ["sports cars", "flatiron school"]
-  all_of_my_favs = @my_favorite_things.concat more_favs
+  @all_my_favs = @my_favorite_things.concat more_favs
 # length not > 2
 end
 
