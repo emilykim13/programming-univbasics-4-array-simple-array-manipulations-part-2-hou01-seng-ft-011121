274ab0c76a80669367c0bb1772bfab20eb96f598
Emily Nguyen <emilyn0614@gmail.com>
2021-01-03 16:54:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,12 +3,10 @@ def using_concat(array1, array2)
   @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   more_favs = ["sports cars", "flatiron school"]
   @all_my_favs = @my_favorite_things.concat more_favs
-# length not > 2
 end
 
 def using_insert(array, element)
   array.insert(4, element)
-# no clue
 end
 
 def using_uniq(array)
