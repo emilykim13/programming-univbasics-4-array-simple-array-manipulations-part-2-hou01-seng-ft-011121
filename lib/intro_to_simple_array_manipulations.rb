3ffae0f2960d6f7af5a2676c6cc63135d9a7cced
Emily Nguyen <emilyn0614@gmail.com>
2021-01-03 16:42:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -24,8 +24,9 @@ def using_flatten(array)
 end
 
 def using_delete(array, string)
-  @instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-  no_offense_steven = @instructors.delete("Steven")
+  instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+  no_offense_steven = instructors.delete("Steven")
+  return instructors
   #binding.pry
 # should work?
 end
